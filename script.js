90922644b78696bbb7b016b4a221703d5a283f23
Chaitanya306 <98307330+Chaitanya306@users.noreply.github.com>
2025-04-22 18:41:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const name=document.getElementById('username')
+const password=document.getElementById('password')
+const checkbox=document.getElementById('checkbox')
+const btn=document.getElementById('existing')
+btn.style.display='none'
+function myFunction(){
+	if(!checkbox.checked){
+		localStorage.clear();
+		alert(`Logged in as ${name.value}`)
+	}
+	if(checkbox.checked){
+		alert(`Logged in as ${name.value}`)
+		localStorage.setItem('username',name.value)
+		localStorage.setItem('password',password.value)
+	}
+}
+if(localStorage.getItem('username')){
+	btn.style.display='block'
+}
+btn.addEventListener('click',function(){
+	alert(`Logged in as ${localStorage.getItem('username')}`);
+})
